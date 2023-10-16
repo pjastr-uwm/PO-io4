57f11d93df0ff151eddfacd04e5dc2b9e3962161
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-10-16 13:08:26 +0200

    D8

---
+++ LAB2/src/D8.java
@@ -0,0 +1,16 @@
+public class D8 {
+
+    public static void main(String[] args) {
+        System.out.println(foo(4));
+    }
+
+    public static int foo(int n)
+    {
+        int temp =0;
+        for(int i=n;i<=2*n;i++)
+        {
+            temp += i;
+        }
+        return temp;
+    }
+}
