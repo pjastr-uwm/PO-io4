fc53e5e2c9393c8802e7769e10fb56d09a4f8d22
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-10-30 13:54:25 +0100

    6

---
+++ lab4/src/TestCar2.java
@@ -0,0 +1,12 @@
+public class TestCar2 {
+
+    public static void main(String[] args) {
+        Car2 car1 = null;
+        if (car1 != null)
+            System.out.println(car1.brand);
+    }
+}
+
+class Car2{
+    public String brand;
+}
