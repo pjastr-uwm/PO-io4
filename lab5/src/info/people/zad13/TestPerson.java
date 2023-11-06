41315b2a0a7f06dcba3360efeec303c1e1ec0e69
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-11-06 13:16:16 +0100

    zad13

---
+++ lab5/src/info/people/zad13/TestPerson.java
@@ -0,0 +1,9 @@
+package info.people.zad13;
+
+class TestPerson {
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Jan", "Kowalski", 22, "Olsztyn");
+        System.out.println(p1.introduceYourself());
+    }
+}
