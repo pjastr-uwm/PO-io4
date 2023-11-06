f98b8f8f06653bfe312b64caba86a720afe48067
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-11-06 13:52:51 +0100

    zad1

---
+++ lab5/src/pl/people/zad1/Person.java
@@ -0,0 +1,15 @@
+package pl.people.zad1;
+
+public class Person {
+    private static int counter;
+
+    private String name;
+
+    public Person(){
+        counter++;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+}
---
+++ lab5/src/pl/people/zad1/TestPerson.java
@@ -0,0 +1,12 @@
+package pl.people.zad1;
+
+public class TestPerson {
+
+    public static void main(String[] args) {
+        System.out.println(Person.getCounter());
+        Person p1 = new Person();
+        System.out.println(Person.getCounter());
+        Person p2 = new Person();
+        System.out.println(Person.getCounter());
+    }
+}
